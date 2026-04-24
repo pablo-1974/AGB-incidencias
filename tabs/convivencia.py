b1b379410e5d6c3636ffb4228fb71655f6b1fba8
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-24 10:06:41 +0200

    Update convivencia.py

--- tabs/convivencia.py
+++ tabs/convivencia.py
@@ -1,9 +1,19 @@
 # tabs/convivencia.py
 import streamlit as st
+
 from tabs.incidents_list import render_incidents_list
+from tabs.incidents_create import render_incident_create
 
 
 def render_convivencia(user: dict):
     st.subheader("🧩 Área de Convivencia")
 
-    render_incidents_list(user, mode="all")
+    tabs = st.tabs(
+        ["📄 Incidencias", "➕ Nueva incidencia"]
+    )
+
+    with tabs[0]:
+        render_incidents_list(user, mode="all")
+
+    with tabs[1]:
+        render_incident_create(user)
