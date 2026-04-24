0ddaab58ee7cf30e03307e2800188a658ed4fd7e
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-24 12:13:07 +0200

    Update convivencia.py

--- tabs/convivencia.py
+++ tabs/convivencia.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from tabs.incidents_list import render_incidents_list
 from tabs.incidents_create import render_incident_create
-
+from tabs.rankings import render_rankings
 
 def render_convivencia(user: dict):
     st.subheader("🧩 Área de Convivencia")
@@ -17,3 +17,10 @@ def render_convivencia(user: dict):
 
     with tabs[1]:
         render_incident_create(user)
+
+
+    # ==========================
+    # RANKINGS
+    # ==========================
+    st.subheader("📊 Rankings")
+    render_rankings(user["role"])
