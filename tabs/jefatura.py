d8bd3812e2d4de75c205e1875861a8ad56b3310a
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-24 10:03:41 +0200

    Update jefatura.py

--- tabs/jefatura.py
+++ tabs/jefatura.py
@@ -1,10 +1,22 @@
 # tabs/jefatura.py
 import streamlit as st
+
 from tabs.incidents_list import render_incidents_list
+from tabs.incidents_create import render_incident_create
+from tabs.incidents_close import render_incidents_close
 
 
 def render_jefatura(user: dict):
     st.subheader("📋 Panel de Jefatura")
 
-    render_incidents_list(user, mode="all")
+    tabs = st.tabs(
+        ["📄 Incidencias", "➕ Nueva incidencia", "✅ Cerrar incidencia"]
+    )
+
+    with tabsrender_incidents_list(user, mode="all")
+
+    with tabsrender_incident_create(user)
+
+    with tabsrender_incidents_close(user)
+
 
