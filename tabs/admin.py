e23aadad35b2ce411c8c8dce276656076dfa9cdf
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-24 10:04:13 +0200

    Update admin.py

--- tabs/admin.py
+++ tabs/admin.py
@@ -1,9 +1,20 @@
 # tabs/admin.py
 import streamlit as st
+
 from tabs.incidents_list import render_incidents_list
+from tabs.incidents_create import render_incident_create
+from tabs.incidents_close import render_incidents_close
 
 
 def render_admin(user: dict):
     st.subheader("🛠️ Administración")
 
-    render_incidents_list(user, mode="all")
+    tabs = st.tabs(
+        ["📄 Incidencias", "➕ Nueva incidencia", "✅ Cerrar incidencia"]
+    )
+
+    with tabsrender_incidents_list(user, mode="all")
+
+    with tabsrender_incident_create(user)
+
+    with tabsrender_incidents_close(user)
