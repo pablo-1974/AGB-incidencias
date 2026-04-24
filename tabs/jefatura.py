eafc451a86f153d92d8eb6064ca35d68f23afc2b
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-24 10:05:54 +0200

    Update jefatura.py

--- tabs/jefatura.py
+++ tabs/jefatura.py
@@ -13,10 +13,11 @@ def render_jefatura(user: dict):
         ["📄 Incidencias", "➕ Nueva incidencia", "✅ Cerrar incidencia"]
     )
 
-    with tabsrender_incidents_list(user, mode="all")
+    with tabs[0]:
+        render_incidents_list(user, mode="all")
 
-    with tabsrender_incident_create(user)
+    with tabs[1]:
+        render_incident_create(user)
 
-    with tabsrender_incidents_close(user)
-
-
+    with tabs[2]:
+        render_incidents_close(user)
