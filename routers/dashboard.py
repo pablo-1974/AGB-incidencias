0f2d9d80365cb270c1f1268158e966725df019e3
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-25 08:15:21 +0200

    Create dashboard.py

---
+++ routers/dashboard.py
@@ -0,0 +1,27 @@
+# routers/dashboard.py
+
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
+
+from context import ctx
+from auth import load_user_dep
+
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+def dashboard(
+    request: Request,
+    user=Depends(load_user_dep),
+):
+    """
+    Dashboard principal de Incidencias.
+    """
+    return request.app.state.templates.TemplateResponse(
+        "dashboard.html",
+        ctx(
+            request,
+            user,
+            title="Panel de control",
+        ),
+    )
