53c4b40e6327acba99ffeb29e831cf89c81ad3ba
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-04-27 22:05:59 +0200

    Update analysis_teacher.py

--- routers/analysis_teacher.py
+++ routers/analysis_teacher.py
@@ -40,7 +40,7 @@ def analysis_teacher(
     # 2. Filtros disponibles
     # --------------------------------------------------
     profesores_raw = get_all_teachers()
-    profesores = [p["teacher_name"] for p in profesores_raw]
+    profesores = [p["name"] for p in profesores_raw]
     
     grupos = get_all_groups()
     alumnos = get_students_by_group(grupo) if grupo else []
